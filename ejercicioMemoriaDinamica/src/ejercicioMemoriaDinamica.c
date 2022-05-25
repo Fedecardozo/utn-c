e681fa242eaceccb201ab2908d0a64db40da08cd
Fede <fedecardozo.a@gmail.com>
2022-05-25 15:00:14 -0300

    Ejercicio remove terminado

--- ejercicioMemoriaDinamica/src/ejercicioMemoriaDinamica.c
+++ ejercicioMemoriaDinamica/src/ejercicioMemoriaDinamica.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAM 5
 #include "utn.h"
 
 int buscarEntero(int* arrayEnteros,int len, int num){
@@ -10,7 +9,7 @@ int buscarEntero(int* arrayEnteros,int len, int num){
 	int retorno = 0;
 	int i;
 
-	if(arrayEnteros != NULL && len > 0)
+	if(arrayEnteros != NULL && len >= 0)
 	{
 
 		for (i = 0; i < len; ++i)
@@ -19,7 +18,9 @@ int buscarEntero(int* arrayEnteros,int len, int num){
 			if(*(arrayEnteros+i) == num)
 			{
 
-				*(arrayEnteros+i)=0;
+				*(arrayEnteros+i)=*(arrayEnteros+len-1);
+				retorno = i;
+
 
 			}
 
@@ -73,6 +74,34 @@ void numAleatorio(int* arrayEnteros,int len){
 
 }
 
+int modificarTamanio(int** arrayEnteros,int* len){
+
+	int retorno=-1;
+
+	if(*arrayEnteros != NULL && len != NULL)
+	{
+		//Realloca el tamanio nuevo
+		int* aux = (int*) realloc(*arrayEnteros,sizeof(int)*(*len-1));
+
+		//printf("\nDIreccion aux: %p\n",aux);
+		//printNum(aux, *len);
+		//printf("\nDIreccion *p: %p",*arrayEnteros);
+		//printf("\nDIreccion solo: %p",arrayEnteros);
+
+		if(aux != NULL)
+		{
+			*len=*len-1;
+			//*arrayEnteros = aux;
+			retorno =0;
+
+		}
+	}
+
+	//printf("\nDIreccion *p: %p\n",*arrayEnteros);
+
+	return retorno;
+}
+
 int main(void) {
 
 	setbuf(stdout,NULL);
@@ -81,19 +110,43 @@ int main(void) {
 	/*Realizar una función que reciba como parámetro un array de enteros,su tamaño y un entero.La
 	función se encargará de buscar el valor entero y borrará la primera ocurrencia del mismo.El array
 	deberá reestructurarse dinámicamente.*/
-
+	int TAM= 5;
 	int* pVector = (int*)calloc(TAM,sizeof(int));
 	int num;
+	int indice;
+
+	printf("\nDIreccion 1: %p\n", pVector);
 
 	numAleatorio(pVector,TAM);
 
 	printNum(pVector, TAM);
 
-	utn_getNumero(&num, "\nIngrese num: ", "\nError: ", 0, 100, 2);
+	if(utn_getNumero(&num, "\nIngrese num: ", "\nError: ", 0, 100, 2)==0)
+	{
 
-	buscarEntero(pVector, TAM, num);
+		indice = buscarEntero(pVector, TAM, num);
 
-	printNum(pVector, TAM);
+	}
+	else
+	{
+		puts("MALA SUERTE");
+	}
+
+
+	if(indice>=0)
+	{
+
+		modificarTamanio(&pVector, &TAM);
+		printNum(pVector, TAM);
+		printf("\nTAM: %d",TAM);
+
+	}
+	else
+	{
+		puts("ALGO FALLO");
+	}
+
+
 
 	return EXIT_SUCCESS;
 }
